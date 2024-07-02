6f1454da9b3982c86fe0c924f2d1974bab625251
Hypercodding <mohammadhammad302@gmail.com>
2024-07-02 16:21:08 +0500

    init

--- app/main.py
+++ app/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .routers import user_routes as user
-from .configurations import configure_cors
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+
+def configure_cors(app: FastAPI):
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],  # This allows all origins, you can restrict it to specific origins if needed
+        allow_credentials=True,
+        allow_methods=["*"],  # This allows all HTTP methods
+        allow_headers=["*"],  # This allows all headers
+    )
+
+# Apply CORS settings
 configure_cors(app)
 
 
