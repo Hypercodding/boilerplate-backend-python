53a6f47d4285d256f1970579d5a8c72a22485fa3
Hypercodding <mohammadhammad302@gmail.com>
2024-07-02 15:54:36 +0500

    init

--- app/configurations/database.py
+++ app/configurations/database.py
@@ -7,7 +7,7 @@ DB_NAME = os.getenv("RAILWAY_DATABASE_NAME", "users")
 DB_USER = os.getenv("POSTGRES_USER")
 DB_PASSWORD = os.getenv("PGPASSWORD")
 DB_HOST = os.getenv("PGHOST")
-DB_PORT = os.getenv("PGPORT", "5432")  # Default port for PostgreSQL
+DB_PORT = os.getenv()  # Default port for PostgreSQL
 
 # Establish database connection
 conn = psycopg2.connect(
@@ -15,7 +15,7 @@ conn = psycopg2.connect(
     user="postgres",
     password=DB_PASSWORD,
     host="roundhouse.proxy.rlwy.net",
-    port=DB_PORT,
+    port=52764,
     cursor_factory=RealDictCursor
 )
 
